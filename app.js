dae4951064f34b657e27293812021a97fe8b03fa
u-ashish <ashish.uppala.93@gmail.com>
2017-09-09 21:22:04 -0400

    added logs for debugging

--- app.js
+++ app.js
@@ -119,51 +119,52 @@ function processMessage(event) {
 }
 
 function findAllGhibliMovies(userId) {
+    console.log("FINDING MOVIES");
+    console.log(userId);
      request(`${BASE_URL}/films`, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            var movieObj = JSON.parse(body);
-            movieObj.forEach(function(movie) {
-                if(movie.Response === "True") {
-                    var query = {user_id: userId};
-                    var update = {
-                        user_id: userId,
-                        title: movie.title,
-                        description: movie.description,
-                        director: movie.director,
-                        producer: movie.producer,
-                        release_date: movie.release_date,
-                        rt_score: movie.rt_score
-                    };
-                    var options = {upsert: true};
-                    Movie.findOneAndUpdate(query, update, options, function(err, mov) {
-                        if (err) {
-                            console.log("Database error: " + err);
-                        } else {
-                            message = {
-                                attachment: {
-                                    type: "template",
-                                    payload: {
-                                        template_type: "generic",
-                                        elements: [{
-                                            title: movie.Title,
-                                            buttons: [{
-                                                type: "postback",
-                                                title: "Yes",
-                                                payload: "Correct"
-                                            }, {
-                                                type: "postback",
-                                                title: "No",
-                                                payload: "Incorrect"
-                                            }]
+            var movie = JSON.parse(body)[0];
+            console.log(movie);
+            if(movie.Response === "True") {
+                var query = {user_id: userId};
+                var update = {
+                    user_id: userId,
+                    title: movie.title,
+                    description: movie.description,
+                    director: movie.director,
+                    producer: movie.producer,
+                    release_date: movie.release_date,
+                    rt_score: movie.rt_score
+                };
+                var options = {upsert: true};
+                Movie.findOneAndUpdate(query, update, options, function(err, mov) {
+                    if (err) {
+                        console.log("Database error: " + err);
+                    } else {
+                        message = {
+                            attachment: {
+                                type: "template",
+                                payload: {
+                                    template_type: "generic",
+                                    elements: [{
+                                        title: movie.Title,
+                                        buttons: [{
+                                            type: "postback",
+                                            title: "Yes",
+                                            payload: "Correct"
+                                        }, {
+                                            type: "postback",
+                                            title: "No",
+                                            payload: "Incorrect"
                                         }]
-                                    }
+                                    }]
                                 }
-                            };
-                            sendMessage(userId, message);
-                        }
-                    });                    
-                }
-            })
+                            }
+                        };
+                        sendMessage(userId, message);
+                    }
+                });                    
+            }
         }
      })
 }
